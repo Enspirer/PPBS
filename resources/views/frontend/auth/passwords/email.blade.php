c1299576d9eb5e9249a27e318ceee89aef48173f
Zajjith Vedha <zajjith@gmail.com>
2021-09-30 16:39:59 +0530

    password reset page

--- resources/views/frontend/auth/passwords/email.blade.php
+++ resources/views/frontend/auth/passwords/email.blade.php
@@ -1,51 +1,46 @@
 @extends('frontend.layouts.app')
 
-@section('title', app_name() . ' | ' . __('labels.frontend.passwords.reset_password_box_title'))
+@section('title', app_name() . ' | ' . __('labels.frontend.auth.login_box_title'))
+
+
+@push('after-styles')
+    <link rel="stylesheet" href="{{ url('css/login.css') }}">
+@endpush
+
 
 @section('content')
-    <div class="row justify-content-center align-items-center">
-        <div class="col col-sm-6 align-self-center">
-            <div class="card">
-                <div class="card-header">
-                    <strong>
-                        @lang('labels.frontend.passwords.reset_password_box_title')
-                    </strong>
-                </div><!--card-header-->
 
-                <div class="card-body">
+    <div class="container-fluid p-0 banner">
+        <div class="container">
+            <div class="row justify-content-center" style="padding-top: 7rem">
+                <div class="col-12 col-md-5 p-4 log">
+                    <h5 class="text-center fw-bold">Password Reset</h5>
+                    <form action="{{route('frontend.auth.login.post')}}" method="post">
+                    {{csrf_field()}}
 
-                    @if(session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+                        <div class="row mt-4">
+                            <div class="mb-3">
+                            
+                                {{ html()->email('email')
+                                    ->class('form-control')
+                                    ->placeholder(__('validation.attributes.frontend.email'))
+                                    ->attribute('maxlength', 191)
+                                    ->required() }}
+                            </div>
                         </div>
-                    @endif
 
-                    {{ html()->form('POST', route('frontend.auth.password.email.post'))->open() }}
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group">
-                                    {{ html()->label(__('validation.attributes.frontend.email'))->for('email') }}
-
-                                    {{ html()->email('email')
-                                        ->class('form-control')
-                                        ->placeholder(__('validation.attributes.frontend.email'))
-                                        ->attribute('maxlength', 191)
-                                        ->required()
-                                        ->autofocus() }}
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
-
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group mb-0 clearfix">
-                                    {{ form_submit(__('labels.frontend.passwords.send_password_reset_link_button')) }}
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
-                    {{ html()->form()->close() }}
-                </div><!-- card-body -->
-            </div><!-- card -->
-        </div><!-- col-6 -->
-    </div><!-- row -->
+                        <div>
+                            <button class="btn w-100 text-white mt-2" style="background-color: #FF9701">Send Password Reset Link</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
+
+@push('after-scripts')
+    @if(config('access.captcha.login'))
+        @captchaScripts
+    @endif
+@endpush
